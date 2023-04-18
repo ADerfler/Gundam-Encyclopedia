5e7a319f4bc45ee70367e889dbdfd26ff81219a1
Anthony Derfler <cderfler@vt.edu>
2023-04-17 20:43:35 -0400

    updated db

---
+++ db/gundams/gundamSchema.js
@@ -0,0 +1,12 @@
+class Gundam {
+    constructor(name, serial, year, image, music, irlYear) {
+        this.name = name
+        this.serial = serial
+        this.year = year
+        this.image = image
+        this.music = music
+        this.irlYear = irlYear
+    }
+}
+
+module.exports = Gundam
---
+++ db/gundams/universalCentury.js
@@ -0,0 +1,220 @@
+const Gundam = require("./gundamSchema")
+
+const ucGundams = {
+    rx78: new Gundam(
+        "Gundam", 
+        "RX-78-2",
+        "0079",
+        "",
+        "",
+        1979
+    ),
+    rx79g: new Gundam(
+        "Gundam Ground Type", 
+        "RX-79[G]",
+        "0079",
+        "",
+        "",
+        1996
+    ),
+    ez8: new Gundam(
+        "Gundam EZ-8", 
+        "RX-79[G]Ez-8",
+        "0079",
+        "",
+        "",
+        1996
+    ),
+    alex: new Gundam(
+        "Alex Gundam", 
+        "RX-78NT-1",
+        "0079",
+        "",
+        "",
+        1989
+    ),
+    fullArmorTB: new Gundam(
+        "Full Armor Gundam (Thunderbolt)", 
+        "FA-78",
+        "0079",
+        "",
+        "",
+        2015
+    ),
+    atlas: new Gundam(
+        "Atlas", 
+        "RX-78AL",
+        "0080",
+        "",
+        "",
+        2017
+    ),
+    zephyranthes: new Gundam(
+        "Zephyranthes", 
+        "GP01",
+        "0083",
+        "",
+        "",
+        1991
+    ),
+    zephFB: new Gundam(
+        "Zephyranthes Full Burnern", 
+        "GP01Fb",
+        "0083",
+        "",
+        "",
+        1991
+    ),
+    physalis: new Gundam(
+        "Physalis", 
+        "GP02A",
+        "0083",
+        "",
+        "",
+        1991
+    ),
+    dendrobium: new Gundam(
+        "Dendrobium", 
+        "GP03",
+        "0083",
+        "",
+        "",
+        1991
+    ),
+    mkii: new Gundam(
+        "MK-II Gundam", 
+        "RX-178",
+        "0086",
+        "",
+        "",
+        1985
+    ),
+    hyakuShiki: new Gundam(
+        "Hyaku Shiki", 
+        "MSN-00100",
+        "0087",
+        "",
+        "",
+        1985
+    ),
+    psyco: new Gundam(
+        "Psyco Gundam", 
+        "MRX-009",
+        "0087",
+        "",
+        "",
+        1985
+    ),
+    zeta: new Gundam(
+        "Zeta Gundam", 
+        "MSZ-006",
+        "0087",
+        "",
+        "",
+        1985
+    ),
+    psyco2: new Gundam(
+        "Psyco Gundam MK-II", 
+        "MRX-010",
+        "0087",
+        "",
+        "",
+        1985
+    ),
+    zz: new Gundam(
+        "ZZ Gundam", 
+        "MSZ-010",
+        "0088",
+        "",
+        "",
+        1986
+    ),
+    nu: new Gundam(
+        "Nu Gundam", 
+        "RX-93",
+        "0093",
+        "",
+        "",
+        1988
+    ),
+    unicorn: new Gundam(
+        "Unicorn", 
+        "RX-0",
+        "0096",
+        "",
+        "",
+        2009
+    ),
+    banshee: new Gundam(
+        "Banshee", 
+        "RX-0",
+        "0096",
+        "",
+        "",
+        2009
+    ),
+    phenex: new Gundam(
+        "Phenex", 
+        "RX-0",
+        "0096",
+        "",
+        "",
+        2013
+    ),
+    narrative: new Gundam(
+        "Narrative Gundam", 
+        "RX-9",
+        "0097",
+        "",
+        "",
+        2018
+    ),
+    xi: new Gundam(
+        "Xi Gundam", 
+        "RX-105",
+        "0105",
+        "",
+        "",
+        1989
+    ),
+    penelope: new Gundam(
+        "Penelope Gundam", 
+        "RX-104FF",
+        "0105",
+        "",
+        "",
+        1989
+    ),
+    f91: new Gundam(
+        "F91 Gundam", 
+        "F91",
+        "0123",
+        "",
+        "",
+        1991
+    ),
+    crossbone: new Gundam(
+        "Crossbone Gundam", 
+        "XM-X1",
+        "0083",
+        "",
+        "",
+        1991
+    ),
+    victory: new Gundam(
+        "Victory Gundam", 
+        "LM312V04",
+        "0153",
+        "",
+        "",
+        1993
+    ),
+    v2: new Gundam(
+        "Gundam V2", 
+        "LM314V21",
+        "0153",
+        "",
+        "",
+        1993
+    )
+}
